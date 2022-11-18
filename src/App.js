b5628997a907ec86471fd52939932ff8a52e7bec
Francisco Vazquez <francisco.v@brillio.com>
2022-11-18 10:50:30 -0600

    Fetch User Data with useContext Hook

--- src/App.js
+++ src/App.js
@@ -5,22 +5,25 @@ import NavBar from "./components/layout/NavBar";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import NotFound from "./Pages/NotFound";
+import { GithubProvider } from "./context/github/githubContext";
 
 const App = () => {
   return (
-    <Router>
-      <div className="flex h-screen flex-col justify-between">
-        <NavBar />
-        <main className="container mx-auto px-3 pb-12">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <GithubProvider>
+      <Router>
+        <div className="flex h-screen flex-col justify-between">
+          <NavBar />
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 };
 
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,27 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useContext } from "react";
+import GithubContext from "../../context/github/githubContext";
 import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
 
 const UserResults = () => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const { users, loading, fetchUsers } = useContext(GithubContext);
 
   useEffect(() => {
     fetchUsers();
-  }, []);
-
-  const fetchUsers = async () => {
-    const res = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
-
-    const data = await res.json();
-    setLoading(false);
-    setUsers(data);
-    console.log(data);
-  };
+  }, [fetchUsers]);
 
   return loading ? (
     <Spinner />
